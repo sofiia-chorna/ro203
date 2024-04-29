f198c4423631e1c3f653261ac1378812f2f188fc
Sofiia Chorna <chernaya.sofiia@gmail.com>
2024-04-29 09:48:36 +0200

    project: fix comments

--- Projet/jeu1/src/generation.jl
+++ Projet/jeu1/src/generation.jl
@@ -11,13 +11,14 @@ Argument
 function generateInstance(N::Array{Int64})
     # Array that will contain the generated grid
     X = zeros(Int, N[1], N[2])
+
     # Counting the total number of monsters at the same time
     G = 0
     V = 0
     Z = 0
     for i in 1:N[1]
         for j in 1:N[2]
-            # choosing randomly the type of the cell (mirror, monster)
+            # Choosing randomly the type of the cell (mirror, monster)
             mirror = rand()
             if mirror < 1 / 5 # probability of having a mirror
                 if rand() < 1 / 2
@@ -28,7 +29,6 @@ function generateInstance(N::Array{Int64})
             else
                 type = rand((1, 2, 3))
                 X[i,j] = type
-
                 if type == 1
                     G += 1
                 elseif type == 2
@@ -40,19 +40,22 @@ function generateInstance(N::Array{Int64})
         end
     end
     C = createPath(N, X)
+
     # Array that contains the values of each path
     Y = zeros(Int, 2 * (N[1] + N[2]))
     for c in 1:2 * (N[1] + N[2])
-        chemin = C[c]
-        for i in 1:size(chemin, 1)
-            cell = chemin[i]
-            # Si il y a un zombie dans la case, on le voit
+        path = C[c]
+        for i in 1:size(path, 1)
+            cell = path[i]
+            # If it is a zombie, we will see it
             if X[cell[1],cell[2]] == 2
                 Y[c] += 1
-            # Si il y a un fantome et qu'on est en reflexion r/r a un miroir
+
+            # If it is a ghost and there is a reflection
             elseif X[cell[1],cell[2]] == 1 && cell[3] == 0
                 Y[c] += 1
-            # Si il y a un vampire et qu'on est en vision directe
+
+            # If it is a vampire and there is no reflection
             elseif X[cell[1],cell[2]] == 3 && cell[3] == 1
                 Y[c] += 1
             end
@@ -71,7 +74,6 @@ Arguments :
 Remark: a grid is generated only if the corresponding output file does not already exist
 """
 function generateDataSet()
-
     # For each grid size considered
     for size in [4, 9, 16, 25]
         N = [size,size]
@@ -79,7 +81,6 @@ function generateDataSet()
         for instance in 1:10
             # Saving the instance to solve and its solution
             fileName = "../data/instance_n" * string(size) * "_" * string(instance) * ".txt"
-
             if !isfile(fileName)
                 game = generateInstance(N)
                 file = open(fileName, "w")
@@ -91,7 +92,3 @@ function generateDataSet()
 
     end
 end
-
-
-
-
